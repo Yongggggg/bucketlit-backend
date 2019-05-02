f16f78bfbe410ab646172c8e79c31cafeecf7169
Yongggggg <ngyong09@gmail.com>
2019-05-02 21:47:30 +0800

    Added function for new item in Bucket List

--- models/bucketlist.py
+++ models/bucketlist.py
@@ -3,11 +3,12 @@ import peewee as pw
 from models.user import User
 
 class Bucketlist(BaseModel):
-    user_id = pw.ForeignKeyField(User, backref="bucketlist")
-    title = pw.CharField()
+    user = pw.ForeignKeyField(User, backref="bucketlist")
+    title = pw.CharField(unique=True)
     description = pw.CharField(max_length=1000, null=True)
     category = pw.CharField()
     longitude = pw.DecimalField(max_digits=9, decimal_places=6, null=True)
     latitude = pw.DecimalField(max_digits=9, decimal_places=6, null=True)
     complete = pw.BooleanField(default=False)
+    completed_by =  pw.DateField()
     address = pw.CharField(null=True)
